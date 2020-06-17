57b6fbee428db2d4b03e059453f9f48c20d3b1db
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-06-17 01:15:38 +0100

    2 - Model core functionality

--- Main.hs
+++ Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+-- priority queue
+-- inbox
+-- information storage
+-- adding a new task
+-- starting work on a task
+
+-- analogous to Code | Data
+data Item = ToDo | Data
+
+data ToDo = ToDo {
+  description :: Text
+} deriving (Show)
+
+-- TODO replace with priority list with good performance characteristics
+type PriorityQueue = [ToDo]
+
+
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
