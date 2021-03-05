f1f63ede61e5eab5c26d511c0216e0c5697c6c44
Ben Fisk <benjfisk@gmail.com>
2021-03-05 09:23:59 +0000

    Add unit test for addToDo

--- test/TestEverythingManager.hs
+++ test/TestEverythingManager.hs
@@ -1,27 +1,30 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 import           Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import Lib.EverythingManager
 import Control.Monad.IO.Class
 
+-- init_everything :: Property
 
-add_todo :: Property
-add_todo =
+-- Start with empty everything. add a todo. ensure todo is successfully added to everything
+testAddToDo :: Property
+testAddToDo =
   property $ do
-  description <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
-  priority <- forAll $ Gen.int (Range.linear 0 100)
-  -- pure ()
-  -- test <- addToDo xs
-  startToDo
-  liftIO $ putStrLn "asdf"
-   
+  -- description <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+  -- priority <- forAll $ Gen.int (Range.linear 0 100) 
+  let
+    description = "TEST"
+    position = 0
+    everything = initEverything
+    result = addToDo description position everything
+  result === Everything { inbox = [], queue  = [ToDo {_description = "TEST"}], notes  = [], habits = [], async = [], thrash = []}
 
-tests :: IO Bool
-tests =
+
+
+
+main :: IO Bool
+main =
   checkParallel $ Group "Test.Example" [
-      ("init_everything", init_everything)
-      ("add_todo", add_todo)
+      -- ("init_everything", init_everything),
+      ("testAddToDo", testAddToDo)
     ]
