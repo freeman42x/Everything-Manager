bb19d8fe7c2e77b43993ab355c1499095831c777
Răzvan Flavius Panda <razvan.panda@gmail.com>
2021-05-01 21:05:44 +0100

    74 - set up haskell.nix

--- shell.nix
+++ shell.nix
@@ -1,37 +1,7 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
-let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, base, containers, generic-lens, hedgehog
-      , lens, stdenv, text
-      }:
-      mkDerivation {
-        pname = "Everything-Manager";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        libraryHaskellDepends = [
-          base containers generic-lens hedgehog lens text
-        ];
-        executableHaskellDepends = [
-          base containers generic-lens hedgehog lens text
-        ];
-        doHaddock = false;
-        description = "System used to optimize the efficiency of achieving goals";
-        license = stdenv.lib.licenses.mit;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+(import ./default.nix).shellFor {
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "latest";
+    haskell-language-server = "latest";
+  };
+}
